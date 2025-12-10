1b3ab1a525ae940a87da7d1952d0899202a8efdb
ftorsuc2608 <ftorsuc2608@g.educaand.es>
2025-12-10 10:09:20 +0100

    Versión F4: Avance adicional tras el reajuste

--- TorrecillasSuchFedericoMiguel_Factorial/pruebaControlVersiones/Factorial.java
+++ TorrecillasSuchFedericoMiguel_Factorial/pruebaControlVersiones/Factorial.java
@@ -27,6 +27,9 @@ public class Factorial {
         System.out.println("RESULTADO - El factorial del número " + num + " es " + factorialRec(num));
 
 	}
+	
+	System.out.println("Proceso completado usando el método recursivo.");
+	
 	private static Scanner sc = new Scanner(System.in);
 
 	
